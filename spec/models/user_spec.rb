584f63474e6fc59b961b9c114a0d280d8c6f0cf5
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-24 15:49:34 +0900

    [WIP]テストコード記述
    # WHAT
    emailがない場合のコード
    
    # WHY

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,3 +8,13 @@ describe User do
     end
   end
 end
+
+describe User do
+  describe '#create' do
+    it "is invalid without a email" do
+      user = User.new(nickname: "aaa", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
+end
