e0e435cf934b6b79e7aaea2654677959aff30d18
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-25 09:20:45 +0900

    [WIP]Add
    あらかじめ、2つのexampleを作成しました。まずは、上の方の「インスタンス変数の値を確かめる」テストコードを書きます。
    
    さて、editアクションが何をしているのかを考えると、まずはtweetsテーブルにレコードが入っていなければいけません。そこで、このexampleの中でtweetsテーブルにレコードを保存します。そのためには、前章で学習したfactory_botを利用します。

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -8,4 +8,12 @@ describe TweetsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'GET #edit' do
+    it "assigns the requested tweet to @tweet" do
+    end
+
+    it "renders the :edit template" do
+    end
+  end
 end
