6df84cfad48baaf5bce9f28883681a8d2d840ec1
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-25 09:39:46 +0900

    [WIP]Add
    5行目で、sortメソッドを利用しています。降順で並び替える方法に関して、詳しくはRuby問題1-5の解説を御覧ください。
    
    これで、indexアクションに関するテストコードは半分完成です。あとは、ページ遷移のexampleですね。

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -27,7 +27,7 @@ describe TweetsController, type: :controller do
     it "populates an array of tweets ordered by created_at DESC" do
       tweets = create_list(:tweet, 3) 
       get :index
-      expect(assigns(:tweets)).to match(tweets)
+      expect(assigns(:tweets)).to match(tweets.sort{ |a, b| b.created_at <=> a.created_at } )
     end
 
     it "renders the :index template" do
