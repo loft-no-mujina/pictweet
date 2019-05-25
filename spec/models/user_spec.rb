c623520a3c485b752e5ea3711de97ed51b7f0439
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-25 09:00:33 +0900

    [WIP]Add
    これまでUser.new()としていた部分をbuild(:user)とすると、同様にUserクラスのインスタンスを作成できるようになっています。buildメソッドの引数にはシンボル型で作成したいクラスの名前を渡しますが、第二引数以降カラム名: 値という形式でハッシュを渡すことで、生成されるインスタンスのプロパティ値を上書きすることができます。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
+
     it "is invalid without a nickname" do
       user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
-  end
-end
 
-describe User do
-  describe '#create' do
     it "is invalid without a email" do
-      user = User.new(nickname: "aaa", email: "", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
+
   end
 end
