1e6a59b2fc2398f1cab03401ebf7652d8cff6eca
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-25 16:58:14 +0900

    [WIP]Error
    FactoryBot::AttributeDefinitionError:
           Attribute already defined: email
    
    重複していたため削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
-
   factory :user do
     nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
+    # email                 {"kkk@gmail.com"}
+    # 上記の記述が下の記述と重複していた
     password              {"00000000"}
     password_confirmation {"00000000"}
     sequence(:email) {Faker::Internet.email}
+    # ランダムなメールを指する。
   end
-
 end
