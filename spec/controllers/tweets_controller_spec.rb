ff415863d9d074fff97f46bc11ff662f8b60eb35
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-25 09:37:43 +0900

    [WIP]Add
    この状態でテストを実行すると、テストはパスします。しかし、このままでは厳密にテストを作成することができていません。tweets_controller.rbを見ると、indexアクションで定義されているインスタンス変数@tweetsは、created_atで降順にソートしているからです。この状態を再現しなければいけません。
    
    現状、create_listメソッドで生成しているtweetのレコードのcreated_atの値は全て同じになってしまっています。これをランダムな値にするために先ほどのFakerを利用します。

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -27,6 +27,7 @@ describe TweetsController, type: :controller do
     it "populates an array of tweets ordered by created_at DESC" do
       tweets = create_list(:tweet, 3) 
       get :index
+      expect(assigns(:tweets)).to match(tweets)
     end
 
     it "renders the :index template" do
