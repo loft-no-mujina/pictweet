4ee5f4b31c357ae584817e248855358253dee1af
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-26 15:07:30 +0900

    [WIP]Error
    # WHAT
    ActionView::Template::Error (undefined method `text' for nil:NilClass):
        1: json.text  @comment.text
        2: json.user_id  @comment.user.id
        3: json.user_name  @comment.user.nickname
    
    発生した
    
    原因：＠抜けいていた
    # WHY

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+    @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
     respond_to do |format|
       format.html { redirect_to tweet_path(params[:tweet_id])  }
       format.json
